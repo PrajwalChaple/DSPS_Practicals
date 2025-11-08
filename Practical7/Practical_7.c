39a8a9e5eb0015058545727dcaaee16a7a57da6a
PrajwalChaple <prajwalchaple14@gmail.com>
2025-11-08 19:16:47 +0530

    Add files via upload

---
+++ Practical7/Practical_7.c
@@ -0,0 +1,79 @@
+// Consider the undirected graph G, consisting of n nodes laid out in a 3 -by- 3 grid: Start searching at node 1, and break ties for exploring the next node based on lower numerical order (i.e. add nodes to a queue low to high, add nodes to a stack high to low). (a) In what order are nodes marked as explored by BFS? (b) In what order are nodes marked as explored by DFS?
+	
+
+#include <stdio.h>
+#define MAX 10
+
+int adj[MAX][MAX];
+int visited[MAX];
+int queue[MAX];
+int n;
+
+void BFS(int start) {
+    int front = -1, rear = -1;
+    int i, j;
+
+    for (i = 1; i <= n; i++)
+        visited[i] = 0;
+
+    printf("\nBFS Traversal starting from vertex %d:\n", start);
+    queue[++rear] = start;
+    visited[start] = 1;
+
+    while (front != rear) {
+        int v = queue[++front];
+        printf("%d ", v);
+
+        for (i = 1; i <= n; i++) {
+            if (adj[v][i] == 1 && visited[i] == 0) {
+                queue[++rear] = i;
+                visited[i] = 1;
+            }
+        }
+    }
+}
+
+void DFS(int v) {
+    int i;
+    visited[v] = 1;
+    printf("%d ", v);
+
+    for (i = 1; i <= n; i++) {
+        if (adj[v][i] == 1 && visited[i] == 0) {
+            DFS(i);
+        }
+    }
+}
+
+int main() {
+    int i, j,s,d,start;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            adj[i][j]=0;
+        }
+    }
+    printf("Enter edges of matrix (%d x %d):\n", n, n);
+    for (i = 1; i <= n; i++) {
+        scanf("%d %d",&s,&d);
+        adj[s][d]=1;   
+        adj[s][d]=1; 
+    }
+
+    printf("Enter starting vertex :\n");
+    scanf("%d", &start);
+
+    
+    BFS(start);
+
+    for (i = 1; i <=n; i++)
+        visited[i] = 0;
+
+printf("\nDFS Traversal starting from vertex %d:\n", start);
+    DFS(start);
+
+    return 0;
+}
